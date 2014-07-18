bc1ffc62a51984af58bddff6fd591518e70dbaed
Adrian <adrian_shs@yahoo.com>
2014-07-18 16:53:22 -0700

    media and audio player

---
+++ vidaud.js
@@ -0,0 +1,48 @@
+
+    		window.onload = function(e){
+    	       document.querySelector('#controls').addEventListener('click',
+    	       function (e){
+    	           var video = document.getElementById('video1');
+    	           var audio = document.getElementById('audio1');
+    	           var background = document.querySelector('.bg');
+    	           switch(e.target.id){
+    	               
+    	               case 'play':
+    	                   video.play();
+    	                   background.classList.toggle('bgChangeone');
+    	                   break;
+    	               case 'pause':
+    	                   video.pause();
+    	                   background.classList.toggle('bgChangetwo');
+    	                   break;
+    	               case 'volumeUp':
+    	                   video.volume+=0.1;
+    	                   background.classList.toggle('bgChangethree');
+    	                   break;
+    	               case 'volumeDown':
+    	                   video.volume-=0.1;
+    	                   background.classList.toggle('bgChangefour');
+    	               
+    	               }
+    	               
+    	           switch(e.target.id){
+    	               case 'playA':
+    	                   audio.play();
+    	                   background.classList.toggle('bgAChangeone');
+    	                   break;
+    	               case 'pauseA':
+    	                   audio.pause();
+    	                   background.classList.toggle('bgAChangetwo');
+    	                   break;
+    	               case 'volumeUpA':
+    	                   audio.volume+=0.1;
+    	                   background.classList.toggle('bgAChangethree');
+    	                   break;
+    	               case 'volumeDownA':
+    	                   audio.volume-=0.1;
+    	                   background.classList.toggle('bgAChangefour');
+    	              }    
+    	            }
+    	          );
+    	    }
+	
